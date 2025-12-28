2074c3c987e076ccb859f3a17d746eb3c71a0988
Allen Su <allenysu@gmail.com>
2025-12-27 17:13:20 -0800

    Initial Commit

---
+++ Pod AI/Pod_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pod_AIApp.swift
+//  Pod AI
+//
+//  Created by Allen Su on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Pod_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
